b659e341010c655c1388c433261de8d300d753fa
Despoyler <arty.sherbyna@gmail.com>
2017-03-16 21:19:49 +0200

    Added game w/ orc

--- Staff/views.py
+++ Staff/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Staff
 # Create your views here.
+def main_menu(request):
+    staffs = Staff.objects.all()
+    print (staffs)
+    title = "Main"
+    context = {
+        "title_docum":title,
+        "staff":staffs
+    }
+    return render(request,"main_menu.html",context)
